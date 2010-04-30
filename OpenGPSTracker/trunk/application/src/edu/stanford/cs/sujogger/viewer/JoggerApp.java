b794c3f8ba31c9e008dd1b14501d6caf8ab25302
James Yang <james04321@rescomp-08-130740.stanford.edu>
2010-04-29 19:03:38 -0700

    basic functionality for the Tracks tab

---
+++ OpenGPSTracker/trunk/application/src/edu/stanford/cs/sujogger/viewer/JoggerApp.java
@@ -0,0 +1,26 @@
+package edu.stanford.cs.sujogger.viewer;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class JoggerApp extends TabActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		TabHost host = getTabHost();
+		host.addTab(host.newTabSpec("tracks").setIndicator("Tracks").
+				setContent(new Intent(this, TrackList.class)));
+		/*
+		host.addTab(host.newTabSpec("badges").setIndicator("Badges").
+				setContent(new Intent(this, null)));
+		host.addTab(host.newTabSpec("groups").setIndicator("Groups").
+				setContent(new Intent(this, null)));
+		host.addTab(host.newTabSpec("feed").setIndicator("Feed").
+				setContent(new Intent(this, null)));*/
+	}
+
+}
